464501167a547b21d2e97dcd31341ddc38fb9442
Adams Korir <adekorir@gmail.com>
2016-08-10 14:46:10 +0000

    changed javascript file and index.html

--- scripts/default.js
+++ scripts/default.js
@@ -1,3 +1,13 @@
 function greetTheWorld() {
     document.getElementById("message").innerHTML = "\"Hello world!\" from adekorir";
 }
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    } else {
+        alert("Hello" + x.toString());
+        return true;
+    }
+}
