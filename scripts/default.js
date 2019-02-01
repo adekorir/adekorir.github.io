9af19ba9439f77624973ec562c10a9c76b30bbc2
Adams Korir <adekorir@gmail.com>
2019-02-01 18:50:58 +0300

    Updated menu button for transition

--- scripts/default.js
+++ scripts/default.js
@@ -1,5 +1,5 @@
 // Select DOM items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
@@ -7,3 +7,27 @@ const navItems = document.querySelectorAll('.nav-item');
 
 // set initial state of menu
 let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // set state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set state
+        showMenu = false;
+    }
+}
